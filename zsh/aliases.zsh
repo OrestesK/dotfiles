3b1f70fcb491c79c5b7779f7a64bfec42c1bad52
OrestesK <oresteskassian@gmail.com>
2026-04-08 17:10:26 -0700

    change: re enable lock

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,10 +1,9 @@
 alias n="nvim" # neovim
 alias fz='nohup xdg-open "$(fzf)" > /dev/null' # open fzf result with default app
 alias lz="lazygit" # lazygit
+alias gs="git-spice"
 
 alias saveshot='f() { wl-paste --type image/png > $1.png };f'
-
-alias rapid='go run /home/orestesk/Coding/Hackatons/RapidTransfer/src/*.go'
 
 alias dupe='nohup wezterm start --cwd $PWD >&/dev/null &'
 
